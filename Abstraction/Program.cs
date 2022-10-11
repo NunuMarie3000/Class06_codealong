dfbcae7443404c73ce6dcd19ce29248d7c2b0a39
Storm O'Bryant <vmarie1997@gmail.com>
2022-10-10 19:35:52 -0500

    class codealong

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -7,6 +7,9 @@ namespace Abstraction
   {
     static void Main(string[] args)
     {
+      // Console.BackgroundColor = ConsoleColor.Green;
+      // Console.ForegroundColor = ConsoleColor.Magenta;
+      Console.ResetColor();
       KidParty johnsParty = new KidParty();
       johnsParty.Setup();
       johnsParty.PlayGames();
